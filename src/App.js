0d0e1bc9c4b831eb8ed262bc71b3010ef5a655aa
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2019-10-30 19:32:00 -0400

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
+// Importing Route
+import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
+    {/* Adding Routes, using exact path for home */}
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
